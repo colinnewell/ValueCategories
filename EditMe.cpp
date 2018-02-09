73ba27ec4abc4ec78a3902d187340b7df41d58e4
Colin Newell <colin.newell@gmail.com>
2018-02-09 20:31:00 +0000

    Exercise

--- EditMe.cpp
+++ EditMe.cpp
@@ -19,17 +19,25 @@ struct T {
 	}
 
 	//Implement a constructor taking a const reference to its own type (T). v should be set to 3.
-	
+	T(const T&) : v(3) {};
 
 	//Implement an operator= also taking a reference to its own type. v should be set to 4.
-	
+	T& operator=(const T&) {
+		v = 4;
+		return *this;
+	}
+
 
 	//Implement a constructor taking an rvalue reference to its own type. v should be set to 5.
-	
+	T(T&&) : v(5) {};
+
 
 	//Implement a static function called TestSix that returns a T and takes a T&.
 	//  The T it returns should have v set to 5.
 	//  Your function MUST NOT just set v to 5.
+	static T TestSix(T& t) {
+		return std::move(t);
+	}
 
 //STOP HERE. DO NOT modify/add/delete anything below this line.
 	T() = default;
@@ -39,4 +47,3 @@ struct T {
 };
 
 
-	
